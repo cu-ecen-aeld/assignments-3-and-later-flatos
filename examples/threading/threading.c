1cf6e95f7696dd695fdb37bf74cfa9ae8fe7eac7
Frank Latos <flatos@duosys.com>
2024-11-23 13:58:53 -0500

    assignment-4-part-1 try 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,31 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct timespec tobtain, trelease;
+    tobtain.tv_sec = thread_func_args->wait_to_obtain_ms / 1000;
+    tobtain.tv_nsec = (thread_func_args->wait_to_obtain_ms % 1000) * 1000000;
+    trelease.tv_sec = thread_func_args->wait_to_release_ms / 1000;
+    trelease.tv_nsec = (thread_func_args->wait_to_release_ms % 1000) * 1000000;
+
+    nanosleep(&tobtain, NULL);
+
+    int ret = pthread_mutex_lock(thread_func_args->mutex);
+    if (ret != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    nanosleep(&trelease, NULL);
+
+    ret = pthread_mutex_unlock(thread_func_args->mutex);
+    if (ret != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +52,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *tdata = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (!tdata)
+        return false;
+
+    // On entry 'mutex' points to a mutex that's been created and locked by caller
+    //      'thread' is an uninitialized pthread_t
+    tdata->mutex = mutex;
+    tdata->wait_to_obtain_ms = wait_to_obtain_ms;
+    tdata-> wait_to_release_ms = wait_to_release_ms;
+
+    int ret = pthread_create(thread, NULL, threadfunc, tdata);
+    if (ret != 0)
+        return false;
+    return true;
 }
 
