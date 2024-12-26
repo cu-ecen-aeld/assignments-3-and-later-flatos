e3a23f402a1077b13353fecc8edc198f526b002c
Frank Latos <flatos@duosys.com>
2024-12-26 16:22:56 -0500

    A8 Try 13

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -88,6 +88,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
     if (copy_to_user(buf, buffptr, retval)) {
         retval = -EFAULT;
         goto out;
+        dsdasdasda;
     }
 
   out:
